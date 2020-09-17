3fc5f111b5f6b88545bff08ce95cc5d083974aa8
Riski Maulana Rahman <Riski_Maulana@D1.lcl>
2020-09-17 17:02:00 +0800

    up version

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ if (!String.prototype.padStart) {
 var RouteProvider = function () {
 	var scriptPath = "js/controllers/";
 	var templatePath = "template/";
-	var v= '5.01'
+	var v= '5.02'
 	this.resolve = function (name) {
 		var route = {};
 		route.templateUrl = templatePath + name + ".html?v="+v;
