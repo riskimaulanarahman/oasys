9a401680e05fe722cff7bd2a3a5a9403b40528e9
Purwanto <purwanto_ihm@itci-hutani.com>
2021-04-05 11:23:11 +0800

    Add Remarks to pdf

--- class/rfcmodule.class.php
+++ class/rfcmodule.class.php
@@ -756,7 +756,8 @@ Class RfcModule extends Application{
 													";
 									$pdfContent .= '<tr><td></td><td style="padding:0in 5.4pt 0in 5.4pt;border-bottom:solid windowtext 1.0pt;">'.$RfcJ->contractorname.'</td></tr>';
 									$pdfContent .= '<tr><td></td><td style="padding:0in 5.4pt 0in 5.4pt;border-bottom:solid windowtext 1.0pt;">'.$RfcJ->contractorname2.'</td></tr>';
-									
+									$pdfContent .= '<tr><td style="padding:0in 5.4pt 0in 5.4pt;">Remarks</td><td style="padding:0in 5.4pt 0in 5.4pt;border-bottom:solid windowtext 1.0pt;">'.wordwrap($RfcJ->remarks, 60, "<br>").'</td></tr>';
+		
 									$pdfContent .= "</table>";
 									$joinx   = "LEFT JOIN tbl_approver ON (tbl_rfcapproval.approver_id = tbl_approver.id) ";					
 									$Rfcapproval = Rfcapproval::find('all',array('joins'=>$joinx,'conditions' => array("rfc_id=?",$doid),'order'=>"tbl_approver.sequence",'include' => array('approver'=>array('employee','approvaltype'))));							
