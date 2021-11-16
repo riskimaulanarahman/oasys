81bfc71a275f18d7d1365a45930b9017296fc467
riskimaulanarahman <riskimaulanarahman@gmail.com>
2021-11-16 22:55:47 +0800

    fixing

--- js/controllers/itsharefolderreport.js
+++ js/controllers/itsharefolderreport.js
@@ -172,7 +172,7 @@
                                 dataField: "approveddoc",
                                 caption:"Approval Doc",
                                 width: 100,
-                                allowFiltering: false,
+                                allowFiltering: true,
                                 allowSorting: false,
                                 formItem: { visible: false},
                                 editorOptions: { 
