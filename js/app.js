5e26ae0b818945d15b6e27cfec822637efbc89cf
riskimaulanarahman <riskimaulanarahman@gmail.com>
2021-12-14 11:48:03 +0800

    version 5.29

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ if (!String.prototype.padStart) {
 var RouteProvider = function () {
 	var scriptPath = "js/controllers/";
 	var templatePath = "template/";
-	var v= '5.28'
+	var v= '5.29'
 	this.resolve = function (name) {
 		var route = {};
 		route.templateUrl = templatePath + name + ".html?v="+v;
