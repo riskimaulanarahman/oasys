5b3577a1246e986be94d47a55fb35b215f26b049
riskimaulanarahman <riskimaulanarahman@gmail.com>
2022-01-04 09:57:11 +0800

    ADD VERSION

--- index.php
+++ index.php
@@ -946,14 +946,14 @@ if (preg_match('/MSIE\s(?P<v>\d+)/i', @$_SERVER['HTTP_USER_AGENT'], $B) || preg_
 	<script language="JavaScript" src="js/lib/FileSaver.min.js" type="text/javascript"></script>
     <!-- <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/globalize/0.1.1/globalize.min.js"></script> -->
     
-	<script language="JavaScript" src="js/app.js?v=5.38" type="text/javascript"></script>
-	<script language="JavaScript" src="js/directive.js?v=5.38" type="text/javascript"></script>
-	<script language="JavaScript" src="js/services.js?v=5.38" type="text/javascript"></script>
-	<script language="JavaScript" src="js/filter.js?v=5.38" type="text/javascript"></script>
-	<script language="JavaScript" src="js/factory.js?v=5.38" type="text/javascript"></script>
-	<script language="JavaScript" src="js/controllers/maincontroller.js?v=5.38" type="text/javascript"></script>
-	<script language="JavaScript" src="js/controllers/login.js?v=5.38" type="text/javascript"></script>
-	<script language="JavaScript" src="js/script.js?v=5.38" type="text/javascript"></script>
+	<script language="JavaScript" src="js/app.js?v=5.39" type="text/javascript"></script>
+	<script language="JavaScript" src="js/directive.js?v=5.39" type="text/javascript"></script>
+	<script language="JavaScript" src="js/services.js?v=5.39" type="text/javascript"></script>
+	<script language="JavaScript" src="js/filter.js?v=5.39" type="text/javascript"></script>
+	<script language="JavaScript" src="js/factory.js?v=5.39" type="text/javascript"></script>
+	<script language="JavaScript" src="js/controllers/maincontroller.js?v=5.39" type="text/javascript"></script>
+	<script language="JavaScript" src="js/controllers/login.js?v=5.39" type="text/javascript"></script>
+	<script language="JavaScript" src="js/script.js?v=5.39" type="text/javascript"></script>
     <!-- <script type="text/javascript" src="https://demo.dashboardpack.com/architectui-html-pro/assets/scripts/main.d810cf0ae7f39f28f336.js"></script> -->
     
 	<script type="text/javascript" src="assets/scripts/main.js"></script>
