eddf02185e2383e3681c76235faf00a85786556a
riskimaulanarahman <riskimaulanarahman@gmail.com>
2023-05-05 08:59:07 +0800

    add v

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ if (!String.prototype.padStart) {
 var RouteProvider = function () {
 	var scriptPath = "js/controllers/";
 	var templatePath = "template/";
-	var v= '5.61'
+	var v= '5.62'
 	this.resolve = function (name) {
 		var route = {};
 		route.templateUrl = templatePath + name + ".html?v="+v;
